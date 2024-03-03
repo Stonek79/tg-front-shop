0665f5bc1d4ff81b31059e1376abfc1f5c47882e
stonek <amit-yurist@mail.ru>
2024-03-03 13:54:35 +0300

    fix tg ready

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,18 +1,7 @@
 'use client'
 
-import {ReactNode, useEffect} from "react";
-import {useTgApp} from "@/lib/hooks/useTgApp";
+import {ReactNode} from "react";
 
 export const PageWrapper = ({ children }: { children?: ReactNode }) => {
-const {tg} = useTgApp()
-
-    useEffect(() => {
-        tg.ready()
-        return () => {
-            tg.close()
-        }
-    })
-
-
     return <div className="page-wrapper">{children}</div>
 }

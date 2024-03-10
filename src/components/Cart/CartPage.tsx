2b44b6ee2db439275a73115468dbe311cf5589ac
stonek <amit-yurist@mail.ru>
2024-03-10 12:31:59 +0300

    add cart page

--- src/components/Cart/CartPage.tsx
+++ src/components/Cart/CartPage.tsx
@@ -1,25 +1,28 @@
 'use client'
 import { Product } from "@/types/product";
 import { getTotalPrice } from "@/lib/helpers/getTotalPrice";
+import { useAddProduct } from "@/lib/hooks/useAddProduct";
 
 export const CartPage = () => {
-    const products: Product[] = JSON.parse(localStorage.getItem('cart') || '[]')
+    const { addedItems} = useAddProduct()
+    const products: Product[] = addedItems
     const total = getTotalPrice(products)
-    return <div>
-        {
-        products.map((prod) => {
-            return <div>
-                <ul>
-                    <li>{prod.title}</li>
-                    <li>{prod.description}</li>
-                    <li><img alt={prod.title} src={prod.thumbnail} /> </li>
-                    <li>{prod.price}</li>
-                </ul>
-            </div>
-        })
-    }
-        <h3>Итого: ` ${total}`</h3>
-        <h3>Всего товаров: ` ${products.length}`</h3>
-    </div>
 
+    return addedItems.length ?
+    <div>
+        {
+            products.map((prod) => {
+                return <div>
+                    <ul>
+                        <li>{ prod.title }</li>
+                        <li>{ prod.description }</li>
+                        <li><img alt={ prod.title } src={ prod.thumbnail }/></li>
+                        <li>{ prod.price }</li>
+                    </ul>
+                </div>
+            })
+        }
+        <h3>Итого: ` ${ total }`</h3>
+        <h3>Всего товаров: ` ${ products.length }`</h3>
+    </div> : <h3>Товары не выбраны</h3>
 }

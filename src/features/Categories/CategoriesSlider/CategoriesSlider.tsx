e0615b6078f22325a6ca4366611422654721cf0f
stonek <amit-yurist@mail.ru>
2024-04-06 22:58:55 +0300

    fix styles

--- src/features/Categories/CategoriesSlider/CategoriesSlider.tsx
+++ src/features/Categories/CategoriesSlider/CategoriesSlider.tsx
@@ -56,7 +56,6 @@ export const CategoriesSlider = memo(() => {
                         </figure>
                     </Link>
                 ))}
-                )
             </div>
         </div>
     )
